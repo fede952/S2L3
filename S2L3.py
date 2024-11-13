7151b23da076ff1269bc5246a89626cf1148baae
Federico Sella <fedesella95@gmail.com>
2024-11-13 14:41:15 +0100

    first commit

---
+++ S2L3.py
@@ -0,0 +1,4 @@
+nome=input("Inserisci città di origine: ").lower()
+nome2=input("Inserisci nome dell'animale: ").lower()
+print("Potresti usare: ",(nome+nome2).capitalize())
+print("Oppure: ",(nome2+nome).capitalize())
